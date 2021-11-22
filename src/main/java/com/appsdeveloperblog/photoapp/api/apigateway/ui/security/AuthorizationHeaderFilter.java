3dd85fd4e9f1f211164eb7eb4afd37691638d32c
Gustavonvp <reinaldo.nunes@mezzow.com>
2021-11-22 07:24:00 -0300

    Implement Method JWT . Closes #8

--- src/main/java/com/appsdeveloperblog/photoapp/api/apigateway/ui/security/AuthorizationHeaderFilter.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/apigateway/ui/security/AuthorizationHeaderFilter.java
@@ -2,19 +2,50 @@ package com.appsdeveloperblog.photoapp.api.apigateway.ui.security;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
 
-    @Override
-    public GatewayFilter apply(Config config) {
-        return null;
-    }
-
     public static class Config {
 
     }
+
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+
+            ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
+
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+            String jwt = authorizationHeader.replace("Bearer", "");
+
+            return chain.filter(exchange);
+        };
+    }
+
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = (ServerHttpResponse) exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return  exchange.getResponse().setComplete();
+    }
+
+
+
+
 }
